fad0325f2e22a709152a758f75dbf0ecd90f325e
Thiagoezeq <ezequielthiago885@gmail.com>
2025-04-02 21:21:18 -0300

    Add files via upload

---
+++ 02 Trabajo Colaborativo/Lagranda,Thiago-TP-02.py.py
@@ -0,0 +1,5 @@
+Nombre="Thiago"
+Apellido="Lagranda"
+Materia="Programación 1"
+Localidad="Malvinas Argentinas"
+print(f"Hola soy {Nombre} {Apellido}, soy de la localidad de {Localidad}, soy alumno de la UTN y estoy en la materia de {Materia}")
